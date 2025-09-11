f2a5b8cd9abebf7e6afaa1e5f45104ced533bd6d
wook-min <gaemicgr@gmail.com>
2025-09-11 21:45:11 +0900

    (2025.09.11.) 스크립트
    유니티1 수업 2
    에셋 스토어에서 에셋 가져오기
    built-in을 urp로 렌더링하기
    Monobehavior 구조 파악
    스크립트와 인스펙터 다루기

---
+++ Assets/Class 002th/Scripts/Ability.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Ability
+{
+    private int strength;
+    public int Strength => strength;
+    private int dexterity;
+    public int Dexterity => dexterity;
+
+    private int intelligence;
+    public int Intelligence => intelligence;
+
+    private int wisdom;
+    public int Wisdom => wisdom;
+
+
+    public void Init(int str, int dex, int Int, int wis)
+    {
+        strength = str;
+        dexterity = dex;
+        intelligence = Int;
+        wisdom = wis;
+    }
+
+}
---
+++ Assets/Class 002th/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    // SRP 단일 책임 원칙
+    public Ability ability;
+
+    private void Awake()
+    {
+        ability = new Ability();
+        ability.Init(10, 10, 10, 10);
+    }
+
+    private void Start()
+    {
+        Debug.Log("STR : " + ability.Strength);
+        Debug.Log("DEX : " + ability.Dexterity);
+        Debug.Log("INT : " + ability.Intelligence);
+        Debug.Log("WIS : " + ability.Wisdom);
+    }
+}
