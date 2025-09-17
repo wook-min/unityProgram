744f217d83ab747e6f3631636cf2000df6c43b93
wook-min <gaemicgr@gmail.com>
2025-09-17 21:44:01 +0900

    (2025.09.17.) 이동 시스템
    유니티1 강의 6
    Transform
    등속 운동 P = P0 + VT
    벡터의 정규화(Normalize)
    유저 인터페이스 (Sprite 2D)
    앵커와 스트레치
    Button 생성

--- Assets/Class 005th (Transform)/Scripts/Pilot.cs
+++ Assets/Class 005th (Transform)/Scripts/Pilot.cs
@@ -2,8 +2,12 @@
 
 public class Pilot : MonoBehaviour
 {
-    // ws >> z축으로 1칸 이동
-    // ad >> x축으로 1칸 이동
+    // P = P0 + vt
+    // 다음위치 = 현재위치 + 방향 X 시간
+
+    [SerializeField] private float speed;
+    [SerializeField] private Vector3 direction;
+
 
     private void Start()
     {
@@ -13,29 +17,45 @@ public class Pilot : MonoBehaviour
 
     private void Update()
     {
-        move();
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        // GetAxis : Smooth Filtering 존재
+        // GetAxisRaw : Smooth Filtering 없음(입력이 멈추면 즉시 값이 0으로)
+
+        direction.Normalize();
+
+        // Time.deltaTime
+        // 이전 프레임이 완료되는 시점부터 현재 프레임이 시작되기까지
+        // 지난 시간을 반환하는 값입니다.
+
+        transform.position = transform.position + direction * Time.deltaTime * speed;
+
+
     }
 
     private void move()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            transform.position += new Vector3(0, 0, 1);
+            transform.position += Vector3.forward;
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            transform.position -= new Vector3(0, 0, 1);
+            transform.position += Vector3.back;
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.position -= new Vector3(1, 0, 0);
+            transform.position += Vector3.left;
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position += new Vector3(1, 0, 0);
+            transform.position += Vector3.right;
         }
+
+        
     }
 }
