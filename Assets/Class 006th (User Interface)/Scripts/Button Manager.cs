24555372a29c513209a8a55d39e2d45b9a8e68c2
wook-min <gaemicgr@gmail.com>
2025-09-18 21:44:55 +0900

    (2025.09.18.) 유저 인터페이스(UI)
    유니티1 강의7
    Vertical Layout Group(버튼이나 각종 ui를 일렬로 정렬해주는 컴포넌트)
    코드로 이벤트 시스템 추가하기
    (Action으로 함수 주소값 저장, 리스트로 버튼을 저장하여 for문을 통해 이벤트를 등록
    이때 클로저 기능으로 인해 index = i를 이용해 꼬임 없이 만들 것, 람다식도 활용)

---
+++ Assets/Class 006th (User Interface)/Scripts/Button Manager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private Button[] buttonList;
+    [SerializeField] private string[] textList = { "Execute", "Option", "Quit"};
+
+    private void Start()
+    {
+        Action[] actions = { Execute, Option, Quit };
+
+        for (int i = 0; i < buttonList.Length; i++)
+        {
+            // closuer(클로저) 기능 때문에 i로만 인덱스를 처리하면 3까지 증가됨
+            int index = i;
+
+            // 람다 문법
+            buttonList[i].onClick.AddListener(() => actions[index]());
+
+            buttonList[i].GetComponentInChildren<Text>().text = textList[index];
+        }
+
+        
+
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Play Start");
+    }
+
+    public void Option()
+    {
+        Debug.Log("Open Option");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Game");
+    }
+}
---
+++ Assets/Class 006th (User Interface)/Scripts/Selectable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Selectable : MonoBehaviour
+{
+    private Button button;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+    }
+
+    public void Enter()
+    {
+        button.GetComponentInChildren<Text>().fontSize = 100;
+    }
+
+    public void Exit()
+    {
+        button.GetComponentInChildren<Text>().fontSize = 75;
+    }
+
+    public void Down()
+    {
+        button.GetComponentInChildren<Text>().fontSize = 50;
+    }
+
+    public void Up()
+    {
+        button.GetComponentInChildren<Text>().fontSize = 100;
+    }
+}
