7c61771f7032aa863476da80764fa572ca92a5d2
wook-min <gaemicgr@gmail.com>
2025-09-22 21:43:58 +0900

    (2025.09.22.) 애니메이터 컨트롤러 시스템 (Animator Controller System)
    유니티1 강의9
    이벤트 같은 시점 중첩시 현상 + 이벤트 함수가 변경될 시 인스펙터로 수정
    막시모 사이트 >> 애니메이션 공짜 goat
    애니메이터 컨트롤러 상태
    Entry : 시작 시 들어가는 상태
    AnyState : 특정 조건에서 들어가는 상태(예 : hp <= 0 일시 die State 전환)
    exit : 끝날때 들어가는 상태
    트랜지션 : State 간을 연결하는 선 (방향성, 조건 등을 달 수 있음)

--- Assets/Class 007th (Animation)/Scripts/Smelter.cs
+++ Assets/Class 007th (Animation)/Scripts/Smelter.cs
@@ -2,8 +2,32 @@
 
 public class Smelter : MonoBehaviour
 {
-    public void Success()
+    [SerializeField] private float progress;
+    [SerializeField] int count;
+
+    public void Create()
     {
-        Debug.Log("Succes");
+        Debug.Log("Create...");
+    }
+
+    // 한번에 입력한 제작 진행률만큼 진행하는 함수
+    public void Process(float progress)
+    {
+        this.progress += progress;
+        Debug.Log($"Progress : {this.progress}%");
+        if (this.progress >= 100f)
+        {
+            Debug.Log("Smelt Complete");
+        }
+
+        this.progress = this.progress % 100f;
+    }
+
+    // 입력한 count 만틈 아이템 강화 시도를 함.
+    public void Enhance(int count)
+    {
+        this.count += count;
+
+        Debug.Log($"Enhance count : {this.count}");
     }
 }
