e39583bd24ba863980f53ca6caa05256bb2ad91d
wook-min <gaemicgr@gmail.com>
2025-09-25 21:44:36 +0900

    (2025.09.25.) 물리 기반 렌더링(PBR)
    유니티1 강의12
    
    instantiate 로 컴파일 중에 게임 오브젝트 생성하기
    컴퓨터의 idle process(컴퓨터 유휴 프로세스)
    이게 많을수록 처리할 수 있는 연산량이 많음. 최대한 적게 쓰는 방향으로 코딩해주어야함.

---
+++ Assets/Class 010th (Instantiate)/Scripts/CreateManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateManager : MonoBehaviour
+{
+    [SerializeField] private int count;
+    [SerializeField] private float time = 5f;
+
+    [SerializeField] private GameObject prefab;
+    
+    private List<GameObject> clones = new();
+    private float timeCheck;
+    private float[] eachTimer = { 0, 0, 0, 0, 0 };
+    private int activeCount = 0;
+
+    private void Start()
+    {
+        timeCheck = 0f;
+
+        if (count > 0)
+        {
+            int middle = count / 2;
+            for (int i = 0; i < count; i++)
+            {
+                var monster = Instantiate(prefab, transform);
+                clones.Add(monster);
+                monster.transform.position = new Vector3(i * 2 - 2 * middle, 0, 0);
+                monster.SetActive(false);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 5.0f)
+        {
+            Debug.Log("Event Start");
+            time = 0.0f;
+        }
+    }
+
+    void Spawn()
+    {
+        timeCheck += Time.deltaTime;
+
+        for (int i = 0; i < eachTimer.Length; i++)
+        {
+            if (clones[i].activeSelf == true)
+            {
+                eachTimer[i] += Time.deltaTime;
+            }
+
+            if (eachTimer[i] >= 4 * time)
+            {
+                clones[i].SetActive(false);
+                Debug.Log($"Monster {i} is ");
+                eachTimer[i] = 0f;
+            }
+        }
+
+        if (timeCheck >= time)
+        {
+            clones[activeCount].SetActive(true);
+            Debug.Log($"Monster {activeCount} is Active");
+            timeCheck = 0f;
+            activeCount = (activeCount + 1) % count;
+        }
+    }
+}
