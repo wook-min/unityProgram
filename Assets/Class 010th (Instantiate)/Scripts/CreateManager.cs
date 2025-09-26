7984a15ff9516394fbe9b7302e4c5d57aed2f8df
wook-min <gaemicgr@gmail.com>
2025-09-26 21:44:32 +0900

    (2025.09.26.) 물리 시스템
    유니티1 강의13
    Coroutine 구현

--- Assets/Class 010th (Instantiate)/Scripts/CreateManager.cs
+++ Assets/Class 010th (Instantiate)/Scripts/CreateManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CreateManager : MonoBehaviour
@@ -8,64 +9,69 @@ public class CreateManager : MonoBehaviour
 
     [SerializeField] private GameObject prefab;
     
-    private List<GameObject> clones = new();
-    private float timeCheck;
-    private float[] eachTimer = { 0, 0, 0, 0, 0 };
-    private int activeCount = 0;
+    private List<GameObject> list = new();
+    private List<int> randomList = new();
+
+    private bool isRunning = true;
+
+    HashSet<int> hash = new();
 
     private void Start()
     {
-        timeCheck = 0f;
+        isRunning = true;
 
+        Create();
+
+        StartCoroutine(Coroutine());
+    }
+
+    
+
+    private void Update()
+    {
+        
+    }
+
+    IEnumerator Coroutine()
+    {
+        while (isRunning)
+        {
+            if (randomList.Count <= 0)
+            {
+                isRunning = false;
+                yield break;
+            }
+
+            Debug.Log("Coroutine Start");
+            
+            yield return new WaitForSeconds(5f);
+
+            int randomIndex = Random.Range(0, randomList.Count);
+
+            int index = randomList[randomIndex];
+            
+            list[index].SetActive(true);
+            randomList.RemoveAt(randomIndex);
+
+            Debug.Log("Coroutine Exit");
+        }
+       
+    }
+
+    void Create()
+    {
         if (count > 0)
         {
             int middle = count / 2;
             for (int i = 0; i < count; i++)
             {
                 var monster = Instantiate(prefab, transform);
-                clones.Add(monster);
+                list.Add(monster);
                 monster.transform.position = new Vector3(i * 2 - 2 * middle, 0, 0);
                 monster.SetActive(false);
+
+                randomList.Add(i);
             }
-        }
-    }
-
-    private void Update()
-    {
-        time += Time.deltaTime;
-
-        if (time >= 5.0f)
-        {
-            Debug.Log("Event Start");
-            time = 0.0f;
-        }
-    }
-
-    void Spawn()
-    {
-        timeCheck += Time.deltaTime;
-
-        for (int i = 0; i < eachTimer.Length; i++)
-        {
-            if (clones[i].activeSelf == true)
-            {
-                eachTimer[i] += Time.deltaTime;
-            }
-
-            if (eachTimer[i] >= 4 * time)
-            {
-                clones[i].SetActive(false);
-                Debug.Log($"Monster {i} is ");
-                eachTimer[i] = 0f;
-            }
-        }
-
-        if (timeCheck >= time)
-        {
-            clones[activeCount].SetActive(true);
-            Debug.Log($"Monster {activeCount} is Active");
-            timeCheck = 0f;
-            activeCount = (activeCount + 1) % count;
         }
     }
 }
