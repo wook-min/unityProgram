e45578d1cf7bf64bd70b04355fdb8f8f9789dc01
wook-min <gaemicgr@gmail.com>
2025-09-30 21:44:19 +0900

    (2025.09.30.) 사운드 시스템
    유니티1 강의15
    rigidBody를 이용한 이동 시스템
    ForceMode 4가지
    AddForce, Acceleration, Impurse, VelocityChange
    물리적이지 않은 충돌 판정 Trigger 시스템
    3가지
    OnTriggerEnter, OnTriggerStay, OnTriggerExit

---
+++ Assets/Class 011th (Psysics)/Scripts/Control.cs
@@ -0,0 +1,71 @@
+﻿using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    // rigidBody를 이용한 이동
+    // 1. AddForce(방향, 힘의 방식)
+    // ForceMode(힘의 방식)
+    // 1) Force : 지속적인 힘, 질량이 적용(mass)
+    // ex) 차량 이동
+    // 2) Acceleration : 지속적인 가속도 (질량 적용 x)
+    // ex) 마법이나 중력같이 지속적인 움직임
+    // 3) Impulse : 순각적인 힘(충격량), 질량 적용 o
+    // ex) 대포나 캐릭터 점프 
+    // 4) VelocityChange : 순각적인 속도 (무게 x)
+    // ex) 대쉬, 텔레포트
+
+    // 물리적인 이동은 FixedUpdate에서 처리하기.(0.02초 고정)
+
+    private Rigidbody RB;
+    private Vector3 direction;
+    [SerializeField] private float addForce = 5f;
+
+    private void Awake()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        control(true);
+    }
+
+    private void FixedUpdate()
+    {
+        RB.AddForce(direction * addForce, ForceMode.Acceleration);
+    }
+
+    private void control()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = Vector3.forward;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector3.left;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector3.back;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector3.right;
+        }
+
+        direction = direction.normalized;
+    }
+
+    private void control(bool t)
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+    }
+}
---
+++ Assets/Class 011th (Psysics)/Scripts/Sensor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Sensor : MonoBehaviour
+{
+    [SerializeField] private Vector3 direction;
+    [SerializeField] private float addForce;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"TriggerEnter At {gameObject.name}");
+        if(other.TryGetComponent<Rigidbody>(out var RB))
+        {
+            RB.AddForce(direction * addForce, ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log($"Trigger Stay At {gameObject.name}");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"Trigger Exit At {gameObject.name}");
+    }
+
+    // 트리거 이벤트 함수
+    // 1. OnTriggerEnter() : 충돌을 했을 때 한번만 호출
+    // 2. OnTriggerStay() : 충돌 중 계속해서 호출(프레임마다)
+    // 3. OnTeriggerExit() : 충돌이 끝났을 때만 호출
+}
