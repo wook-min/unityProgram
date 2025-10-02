5b300716f701e504a52159bbecffb86f008f3aae
wook-min <gaemicgr@gmail.com>
2025-10-02 21:43:30 +0900

    (2025.10.02.) 광선 검출 시스템(Ray Cast System)
    유니티1 강의17
    RayCast
    Ray : Camera.main.ScreenPointToRay(Input.MousePoint)로 참조
    RayCastHit : Ray가 닿이는 곳, Physice.Raycast(ray, out rayCastHit)으로 사용

---
+++ Assets/Class 012th (Ray-Cast)/Scripts/Mouse.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Mouse : MonoBehaviour
+{
+    [SerializeField] private Ray ray;
+    [SerializeField] private RaycastHit rayCastHit;
+    [SerializeField] private float distance;
+    [SerializeField] private Texture2D texture2D;
+    private void Awake()
+    {
+        distance = Mathf.Infinity;
+        Cursor.SetCursor(texture2D, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 마우스로 찍은 포지션 값을 넣으면, 공간을 변형해서 3차원 hit로 바꿔줌
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(ray.origin, ray.direction * distance, Color.green);
+
+            if (Physics.Raycast(ray, out rayCastHit, 100))
+            {
+                Debug.Log(rayCastHit.collider.name);
+
+                Debug.DrawLine(ray.origin, rayCastHit.point, Color.red);
+            }
+        }
+    }
+}
